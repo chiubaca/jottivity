e7959b6ac61a34249959a7c326c2ef5e8a77c1f3
Alex Chiu <alexchiu11@gmail.com>
2020-06-15 17:20:38 +0100

    Firebase config module for easy reuse across other functions

---
+++ functions/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: "jottivity-live.firebaseapp.com",
+  databaseURL: "https://jottivity-live.firebaseio.com",
+  projectId: "jottivity-live",
+  storageBucket: "jottivity-live.appspot.com",
+  messagingSenderId: "718665282013",
+  appId: "1:718665282013:web:69b4feb386ea031ca30634"
+};
