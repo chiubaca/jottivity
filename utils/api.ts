126d85da0ecab1d16fb6937d0b379e58603c08c2
Alex Chiu <alexchiu11@gmail.com>
2020-06-19 20:59:35 +0100

    console logs for netlify troubleshooting

--- utils/api.ts
+++ utils/api.ts
@@ -9,13 +9,19 @@ export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
   $axios = axiosInstance;
 
   if (process.env.NODE_ENV === "development") {
+    console.log("We're in dev")
     $axios.setBaseURL(process.env.DEV_API_BASE_URL as string);
+    console.log($axios);
     return;
   }
-
-  $axios.setBaseURL(
-    document.location.hostname + "/.netlify/functions/register"
-  );
+  if (process.env.NODE_ENV === "production") {
+    console.log("We're in prod")
+    $axios.setBaseURL(
+      document.location.hostname + "/.netlify/functions/register"
+    );
+    console.log($axios);
+    return;
+  }
 }
 
 export { $axios };
