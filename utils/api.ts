47c565c2e0d2dca000e9c923798be7debfd8fa1a
Alex Chiu <alexchiu11@gmail.com>
2020-06-21 18:00:56 +0100

    Configured axios to only throw errors on https status codes which are 500 or above

--- utils/api.ts
+++ utils/api.ts
@@ -9,18 +9,20 @@ export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
   $axios = axiosInstance;
 
   if (process.env.NODE_ENV === "development") {
-    console.log("We're in dev")
+    console.log("We're in dev");
     $axios.setBaseURL(process.env.DEV_API_BASE_URL as string);
+    // only throw error if status code is 500 or above
+    $axios.defaults.validateStatus = (status: number) => status <= 500;
     console.log($axios);
     return;
   }
   if (process.env.NODE_ENV === "production") {
-    console.log("We're in prod")
+    console.log("We're in prod");
     $axios.setBaseURL(
       "https://" + document.location.hostname + "/.netlify/functions/"
     );
+    $axios.defaults.validateStatus = (status: number) => status <= 500; 
     console.log($axios);
-    return;
   }
 }
 
