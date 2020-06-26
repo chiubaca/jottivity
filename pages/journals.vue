da31913ec4421c433b0077a32da8d68cb4b4793b
Alex Chiu <alexchiu11@gmail.com>
2020-06-26 12:19:13 +0100

    Placeholder journal page with auth middleware for security

---
+++ pages/journals.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="journals">
+    <div class="container--center">
+      <h1>Your Journals</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  middleware: "authenticated"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
