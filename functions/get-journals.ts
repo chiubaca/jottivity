1c8d45251310a43e356eca5859d8cd0a44e4da89
Alex Chiu <alexchiu11@gmail.com>
2020-07-06 16:21:15 +0100

    Reenabled JWT check

--- functions/get-journals.ts
+++ functions/get-journals.ts
@@ -23,13 +23,13 @@ export const handler = async function(
   try {
     console.log("getting journals...");
     // Extract JWT from header
-    // const JWT = event.headers.authorization;
-    // console.log(JWT);
-    // // Verify JWT, if user deleted, or JWT is invalid, this will throw an error
-    // const user: admin.auth.DecodedIdToken = await admin
-    //   .auth()
-    //   .verifyIdToken(JWT, true);
-    // console.log("user obj",user);
+    const JWT = event.headers.authorization;
+    console.log(JWT);
+    // Verify JWT, if user deleted, or JWT is invalid, this will throw an error
+    const user: admin.auth.DecodedIdToken = await admin
+      .auth()
+      .verifyIdToken(JWT, true);
+    console.log("user obj",user);
     // TODO Could check for custom claim for additional security logic here
     // see - https://firebase.google.com/docs/auth/admin?hl=en
 
