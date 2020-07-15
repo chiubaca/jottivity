706c5fb2c9a0f8d4a0b59e885bc82a0031493e4d
Alex Chiu <alexchiu11@gmail.com>
2020-07-15 18:13:42 +0100

    Refactor and comments so pattern follows crud API

--- functions/email-login/email-login.ts
+++ functions/email-login/email-login.ts
@@ -1,10 +1,10 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import { APIGatewayProxyEvent, APIGatewayProxyCallback } from "aws-lambda";
-import { JUser, JLoginCrendentials, firebaseExt } from "../types";
-import { firebaseConfig } from "../firebase";
+import { JUser, JLoginCrendentials, firebaseExt } from "../../types";
+import { firebaseConfig } from "../../firebase";
+
 // Prevents firebase from initilising more than once
-// TODO: find a way to remove this.
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
@@ -14,53 +14,54 @@ export const handler = async function(
   _context: any,
   callback: APIGatewayProxyCallback
 ) {
-  if (event.httpMethod !== "POST") {
-    callback(null, {
-      statusCode: 405,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ error: "POST requests only" })
-    });
-  }
-  const loginCreds: JLoginCrendentials = JSON.parse(event.body as string);
-
-  if (loginCreds.email === "" || loginCreds.password === "") {
-    return callback(null, {
-      statusCode: 400,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        error: { message: "email or password can not be empty" }
-      })
-    });
-  }
-
   try {
-    await firebase
-      .auth()
-      .signInWithEmailAndPassword(loginCreds.email, loginCreds.password);
+    if (event.httpMethod === "POST") {
+      const loginCreds: JLoginCrendentials = JSON.parse(event.body as string);
 
-    const User: firebase.User = firebase.auth().currentUser as firebase.User;
+      // If credentials are empty return 400 error
+      if (loginCreds.email === "" || loginCreds.password === "") {
+        return callback(null, {
+          statusCode: 400,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            error: { message: "email or password can not be empty" }
+          })
+        });
+      }
 
-    const userJson: firebaseExt.UserJSON = User.toJSON() as firebaseExt.UserJSON;
+      // Parse the credential if they are available
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(loginCreds.email, loginCreds.password);
 
-    const user: JUser = {
-      tokens: {
-        refreshToken: userJson.stsTokenManager.refreshToken,
-        accessToken: userJson.stsTokenManager.accessToken,
-        expirationTime: userJson.stsTokenManager.expirationTime
-      },
-      email: userJson.email,
-      emailVerified: userJson.emailVerified,
-      lastLoginAt: userJson.lastLoginAt,
-      createdAt: userJson.createdAt,
-      uid: userJson.uid,
-      username: userJson.displayName
-    };
-
-    callback(null, {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ ...user })
-    });
+      const User: firebase.User = firebase.auth().currentUser as firebase.User;
+      const userJson: firebaseExt.UserJSON = User.toJSON() as firebaseExt.UserJSON;
+      // Setup the user object to be sent to client
+      const user: JUser = {
+        tokens: {
+          refreshToken: userJson.stsTokenManager.refreshToken,
+          accessToken: userJson.stsTokenManager.accessToken,
+          expirationTime: userJson.stsTokenManager.expirationTime
+        },
+        email: userJson.email,
+        emailVerified: userJson.emailVerified,
+        lastLoginAt: userJson.lastLoginAt,
+        createdAt: userJson.createdAt,
+        uid: userJson.uid,
+        username: userJson.displayName
+      };
+      callback(null, {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...user })
+      });
+    } else {
+      callback(null, {
+        statusCode: 405,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ error: "Invalid Operation" })
+      });
+    }
   } catch (error) {
     callback(null, {
       statusCode: 400,
