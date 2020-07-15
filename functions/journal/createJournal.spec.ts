d182c59fc91d35fd42802a55e92e787607f14ca0
Alex Chiu <alexchiu11@gmail.com>
2020-07-15 17:36:12 +0100

    Initial test for createJournal API. Still dont understand unit testing well enough to proceed

--- functions/journal/createJournal.spec.ts
+++ functions/journal/createJournal.spec.ts
@@ -1,4 +1,6 @@
 import LambdaTester from "lambda-tester";
+// import * as admin from "firebase-admin";
+// import { initFirebaseAdmin } from "../helpers/initFirebase";
 import createJournal from "./createJournal";
 
 describe("create journals endpoint", () => {
@@ -15,4 +17,20 @@ describe("create journals endpoint", () => {
         });
       });
   });
+
+  // test("JWT is being verified", async () => {
+  //     initFirebaseAdmin();
+
+  //   await LambdaTester(createJournal)
+  //     .event({ headers: { authorization: "12345" } })
+  //     .expectResult((result: any) => {
+  //       return expect(result).toEqual({
+  //         body: '{"error":"Unauthorised, no token was provided"}',
+  //         headers: {
+  //           "Content-Type": "application/json"
+  //         },
+  //         statusCode: 401
+  //       });
+  //     });
+  // });
 });
