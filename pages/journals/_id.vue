cff63148a2b3ff37739c2eb6cab162d269480485
Alex Chiu <alexchiu11@gmail.com>
2020-08-01 18:45:35 +0100

    Setup UI input for adding new posts

--- pages/journals/_id.vue
+++ pages/journals/_id.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
     <h1>Your Posts</h1>
+    <div class="fixed">
+      <NewPostButton
+        button-text="Add A New Post"
+        @create="addNewPost($event)"
+      />
+    </div>
   </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import Vue from "vue";
+import NewPostButton from "@/components/NewPostButton.vue";
+
+export default Vue.extend({
+  components: {
+    NewPostButton
+  },
+  methods: {
+    addNewPost(newPost: unknown) {
+      console.log("Adding new Post", newPost);
+    }
+  }
+});
+</script>
 
 <style scoped></style>
