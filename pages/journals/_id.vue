636e9aa2e86aa7fc2efc5ba508539ce95bad890b
Alex Chiu <alexchiu11@gmail.com>
2020-08-02 17:30:03 +0100

    Utalise current Journal meta state in component

--- pages/journals/_id.vue
+++ pages/journals/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Your Posts</h1>
+    <h1>Your Posts | {{journalMeta.name}}</h1>
     <div class="fixed">
       <NewPostButton
         button-text="Add A New Post"
@@ -12,16 +12,22 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { mapMutations, mapState } from "vuex";
 import NewPostButton from "@/components/NewPostButton.vue";
 
 export default Vue.extend({
+  middleware: "setJournalState",
   components: {
     NewPostButton
   },
   methods: {
+    ...mapMutations("posts", ["ADD_POST"]),
     addNewPost(newPost: unknown) {
       console.log("Adding new Post", newPost);
     }
+  },
+  computed:{
+    ...mapState("Posts", ["journalMeta"])
   }
 });
 </script>
