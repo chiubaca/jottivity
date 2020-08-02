78ff9a11192d2dafd7fce6adebc12203fae2ccfc
Alex Chiu <alexchiu11@gmail.com>
2020-08-02 18:02:35 +0100

    Set up Mutations  add new posts to vuex and getter for displaying posts in post page

--- store/Posts.ts
+++ store/Posts.ts
@@ -1,17 +1,8 @@
+import Vue from "vue";
 import { Module, VuexModule, Action, Mutation } from "vuex-module-decorators";
 import store from "vuex";
-import { $axios } from "~/utils/api";
+import { JAllPosts, JPost, JJournal } from "@/types";
 // import { $axios } from "~/utils/api";
-
-type JAllPosts = {
-  [postID: string]: JPost[];
-};
-
-type JPost = {
-  title: string;
-  contents: string;
-  createAt: number;
-};
 
 @Module({
   namespaced: true,
@@ -19,27 +10,49 @@ type JPost = {
   store: store as any
 })
 export default class Posts extends VuexModule {
-  posts: JAllPosts = {};
+  allPosts: JAllPosts = {};
+
+  journalMeta: JJournal | undefined = undefined;
 
   @Mutation
   SETUP_POSTS(journalId: string) {
-    this.posts[journalId] = [];
+    Vue.set(this.allPosts, journalId, []);
   }
 
   @Mutation
   DELETE_ALL_POSTS(journalId: string) {
-    delete this.posts[journalId];
+    delete this.allPosts[journalId];
+  }
+
+  @Mutation
+  ADD_POST(postPayload: { journalId: string; post: JPost }) {
+    const { journalId, post } = postPayload;
+    this.allPosts[journalId].push(post);
+  }
+
+  @Mutation
+  SET_JOURNAL_META(journal: JJournal) {
+    this.journalMeta = journal;
+  }
+
+  get allPostInCurrentJournal() {
+    if (!this.journalMeta?.id) {
+      return [];
+    }
+    return this.allPosts[this.journalMeta.id];
   }
 
   @Action({ rawError: true })
   async createPost(post: JPost) {
-    const tokens = this.context.rootState.Auth.user.tokens;
+    const tokens = await this.context.rootState.Auth.user.tokens;
     try {
-      const resp = await $axios.$post(
-        "post",
-        { ...post },
-        { headers: { Authorization: tokens.accessToken } }
-      );
+      // const resp = await $axios.$post(
+      //   "post",
+      //   { ...post },
+      //   { headers: { Authorization: tokens.accessToken } }
+      // );
+      // return resp;
+      return tokens;
     } catch (err) {
       console.error("server error creating journal", err);
       return err;
