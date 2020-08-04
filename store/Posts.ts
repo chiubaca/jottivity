36b68afcde49f8b4888e3a33c8d01d937c0f33f4
Alex Chiu <alexchiu11@gmail.com>
2020-08-04 15:14:31 +0100

    Initial implementation for adding new posts and getters for all posts and current journal info

--- store/Posts.ts
+++ store/Posts.ts
@@ -10,24 +10,30 @@ import { JAllPosts, JPost, JJournal } from "@/types";
   store: store as any
 })
 export default class Posts extends VuexModule {
-  allPosts: JAllPosts = {};
+  posts: JAllPosts = {};
 
   journalMeta: JJournal | undefined = undefined;
 
   @Mutation
   SETUP_POSTS(journalId: string) {
-    Vue.set(this.allPosts, journalId, []);
+    Vue.set(this.posts, journalId, []);
   }
 
   @Mutation
   DELETE_ALL_POSTS(journalId: string) {
-    delete this.allPosts[journalId];
+    delete this.posts[journalId];
   }
 
   @Mutation
   ADD_POST(postPayload: { journalId: string; post: JPost }) {
     const { journalId, post } = postPayload;
-    this.allPosts[journalId].push(post);
+
+    // create the Journal namespace for the posts if its not there yet.
+    if (!this.posts[journalId]) {
+      Vue.set(this.posts, journalId, []);
+      this.posts[journalId].push(post);
+    }
+    this.posts[journalId].push(post);
   }
 
   @Mutation
@@ -35,27 +41,35 @@ export default class Posts extends VuexModule {
     this.journalMeta = journal;
   }
 
+  get allPosts() {
+    return this.posts;
+  }
+
   get allPostInCurrentJournal() {
     if (!this.journalMeta?.id) {
       return [];
     }
-    return this.allPosts[this.journalMeta.id];
+    return this.posts[this.journalMeta.id];
   }
 
-  @Action({ rawError: true })
-  async createPost(post: JPost) {
-    const tokens = await this.context.rootState.Auth.user.tokens;
-    try {
-      // const resp = await $axios.$post(
-      //   "post",
-      //   { ...post },
-      //   { headers: { Authorization: tokens.accessToken } }
-      // );
-      // return resp;
-      return tokens;
-    } catch (err) {
-      console.error("server error creating journal", err);
-      return err;
-    }
+  get currentJournalInfo() {
+    return this.journalMeta;
   }
+
+  // @Action({ rawError: true })
+  // async createPost(post: JPost) {
+  //   const tokens = await this.context.rootState.Auth.user.tokens;
+  //   try {
+  //     // const resp = await $axios.$post(
+  //     //   "post",
+  //     //   { ...post },
+  //     //   { headers: { Authorization: tokens.accessToken } }
+  //     // );
+  //     // return resp;
+  //     return tokens;
+  //   } catch (err) {
+  //     console.error("server error creating journal", err);
+  //     return err;
+  //   }
+  // }
 }
