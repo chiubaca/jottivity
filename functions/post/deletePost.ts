450ceab6aec5d33e6cb4e56b5e3a83c6a9cf9ba3
Alex Chiu <alexchiu11@gmail.com>
2020-08-08 10:26:24 +0100

    inital implementation of delete post

--- functions/post/deletePost.ts
+++ functions/post/deletePost.ts
@@ -20,10 +20,30 @@ export default async function createJournal(
     // Verify JWT, if user deleted, or JWT is invalid, this will throw an error
     await admin.auth().verifyIdToken(JWT, true);
 
+
+    // extract post id from query string
+    const queryParam = event?.queryStringParameters;
+
+    if (!queryParam?.id) {
+      return callback(null, {
+        statusCode: 401,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ error: "no post id was provided" })
+      });
+    }
+    const postId = queryParam.id;
+    await admin
+      .firestore()
+      .collection("posts")
+      .doc(postId)
+      .delete();
+
     return callback(null, {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ success: "Endpoint not implemented yet" })
+      body: JSON.stringify({
+        message: `deleted post ID ${postId}`
+      })
     });
   } catch (error) {
     console.error("There was an error creating a new journal", error);
@@ -32,5 +52,8 @@ export default async function createJournal(
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ error })
     });
+  } finally {
+    // End the firebase instance otherwise netlify function will hang
+    await admin.app().delete();
   }
 }
