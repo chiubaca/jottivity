ed86c719b4df868350cde8a5b5ed5f4c930b064d
Alex Chiu <alexchiu11@gmail.com>
2020-08-22 19:58:12 +0100

    Initial implementation of PostCard component

---
+++ components/PostCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post">
+    <h1>{{ post.title }}</h1>
+    {{ post.createdAt }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { JPost } from "@/types";
+
+@Component
+export default class PostCard extends Vue {
+  @Prop({ required: true }) readonly post!: JPost;
+}
+</script>
+
+<style scoped>
+.post {
+  color: #2c3e50;
+  box-shadow: 0px 0px 13px #7d7d7d;
+  margin: 15px;
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>
