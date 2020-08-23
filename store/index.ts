369259a4a507739d221d4cd75289116d07de7b47
Alex Chiu <alexchiu11@gmail.com>
2020-08-23 15:35:09 +0100

    Updated import syntax

--- store/index.ts
+++ store/index.ts
@@ -2,4 +2,4 @@ import { Store } from "vuex";
 import { initialiseStores } from "@/utils/store-accessor";
 const initializer = (store: Store<any>) => initialiseStores(store);
 export const plugins = [initializer];
-export * from "~/utils/store-accessor";
+export * from "@/utils/store-accessor";
