d7b58de2ff0e72913cd52666c1434a50c8f36787
Alex Chiu <alexchiu11@gmail.com>
2020-08-31 15:37:51 +0100

    Simple theme picker

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <nuxt />
+    <div>
+      <h1>Color mode: {{ $colorMode.value }}</h1>
+      <select v-model="$colorMode.preference">
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
