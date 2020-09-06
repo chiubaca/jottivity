a6b17ba424280ababfef196fd1d0b5d385a13280
Alex Chiu <alexchiu11@gmail.com>
2020-09-06 18:48:21 +0100

    Add tag store to app

--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -2,13 +2,16 @@ import { Store } from "vuex";
 import { getModule } from "vuex-module-decorators";
 import PostStore from "@/store/Posts";
 import AuthStore from "@/store/Auth";
+import TagStore from "@/store/Tags";
 
 let Posts: PostStore;
 let Auth: AuthStore;
+let Tag: TagStore;
 
 function initialiseStores(store: Store<any>): void {
   Posts = getModule(PostStore, store);
   Auth = getModule(AuthStore, store);
+  Tag = getModule(TagStore, store);
 }
 
-export { initialiseStores, Posts, Auth };
+export { initialiseStores, Posts, Auth, Tag };
